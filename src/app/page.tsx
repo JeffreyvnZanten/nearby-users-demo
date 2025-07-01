e4dc2cf4d072dbae0dc2e0dab3ec2c308561a92e
Jef van Zanten <j.van.zanten@gmail.com>
2025-07-01 15:56:17 +0200

    Added a welcome text with the users name

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,7 @@ export default async function Home() {
   return (
     <div className="flex flex-col w-full h-screen justify-center items-center">
       <div className="border-1 flex flex-col items-center justify-center min-h-[10vh] w-[20em]">
-        Home
+        Welkom {session.user.name}
       </div>
     </div>
   );
